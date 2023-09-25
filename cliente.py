282f5d3ec96f1a8fcf14091683390c563b0e2947
Joana Cabral <joanacabralmartins@outlook.com>
2023-09-25 00:41:14 -0300

    lado do cliente

---
+++ cliente.py
@@ -0,0 +1,21 @@
+import xmlrpc.client
+
+def main():
+    proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+
+    while True:
+        print("Escolha: pedra, papel, tesoura ou sair")
+        player_choice = input("> ").lower()
+        
+        if player_choice == 'sair':
+            break
+        
+        if player_choice not in ['pedra', 'papel', 'tesoura']:
+            print("Opção inválida. Tente novamente.")
+            continue
+        
+        result = proxy.play(player_choice)
+        print(result)
+
+if __name__ == "__main__":
+    main()
